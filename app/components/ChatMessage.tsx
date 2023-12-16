374b8a209233e6bb3019699b6c9d73f8998900af
kevinkons <kevin.kons@hotmail.com>
2023-12-16 11:21:52 -0300

    Chat ChatMessage component

---
+++ app/components/ChatMessage.tsx
@@ -0,0 +1,29 @@
+import { twMerge } from 'tailwind-merge';
+
+export enum MessageType {
+  USER_MESSAGE,
+  BOT_MESSAGE
+}
+
+export default function ChatMessage({ message, messageType }: { message: string, messageType: MessageType }) {
+  return (
+    <div className="chat-message">
+      <div className={twMerge("flex items-end", messageType === MessageType.USER_MESSAGE && "justify-end")}>
+        <div className={twMerge("flex flex-col space-y-2 text-xs max-w-xs mx-2", `${messageType === MessageType.BOT_MESSAGE ? "order-2 items-start" : "order-1 items-end"}`)}>
+          <div>
+            <span
+              className={twMerge(
+                "px-4 py-2 rounded-lg inline-block",
+                `${messageType == MessageType.BOT_MESSAGE ? "rounded-bl-none bg-gray-300 text-gray-600" : "rounded-br-none bg-blue-600 text-white"}`,
+              )}
+            >
+              {message}
+            </span>
+          </div>
+        </div>
+        <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
+      </div>
+
+    </div>
+  )
+} 
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,16 @@
 import Image from 'next/image'
 import SendMessageBox from './components/SendMessageBox'
+import ChatHeader from './components/ChatHeader'
+import ChatMessage, { MessageType } from './components/ChatMessage'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
-        <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
-          <div className="relative flex items-center space-x-4">
-            <div className="relative">
-              <span className="absolute text-green-500 right-0 bottom-0">
-                <svg width="20" height="20">
-                  <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
-                </svg>
-              </span>
-              <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="" className="w-10 sm:w-16 h-10 sm:h-16 rounded-full" />
-            </div>
-            <div className="flex flex-col leading-tight">
-              <div className="text-2xl mt-1 flex items-center">
-                <span className="text-gray-700 mr-3">Artificial Accountant</span>
-              </div>
-              <span className="text-lg text-gray-600">Here to help you</span>
-            </div>
-          </div>
-          <div className="flex items-center space-x-2">
-            <button type="button" className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
+        <ChatHeader />
         <div id="messages" className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
-          <div className="chat-message">
-            <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">Can be verified on any platform using docker</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
-            </div>
-          </div>
-          <div className="chat-message">
-            <div className="flex items-end justify-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Your error message says permission denied, npm global installs must be given root privileges.</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
-            </div>
-          </div>
+          <ChatMessage messageType={MessageType.USER_MESSAGE} message='Hi how are you?' />
+          <ChatMessage messageType={MessageType.BOT_MESSAGE} message='Good, thanks for asking.' />
         </div>
         <SendMessageBox />
       </div>
